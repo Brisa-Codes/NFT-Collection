6f7c7ec0417be5c01f62b574a6b66bd4517d8f13
Brisa-Codes <brisamukunde1@gmail.com>
2022-10-18 10:17:22 +0300

    update: new contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xFfdd13CE312f421E9E327d3c64D936Cf1554C834";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-gilt-gamma.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
